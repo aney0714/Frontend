182978c5c249efe3f0877001e4efe3959fefdc23
aney0714 <gimhh770@gmail.com>
2025-11-25 13:26:16 +0900

    김예나 nav바 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {White_Circle,Pink_Circle,Black_Circle,NAV_Image} from "../styles/NavBar";
+import HOME from "../img/home.png";
+import MESSAGE from "../img/message.png";
+import SECRET from "../img/secret.png";
+import PHONE from "../img/phone.png";
+
 
 const NavBar = () =>{
 
     return(
-        <div>
-            
-        </div>
+        <White_Circle>
+            <Pink_Circle>
+                <Black_Circle>
+                 <NAV_Image src={PHONE}/> {/*링크 나중에 걸 예정*/}
+                 <NAV_Image src={HOME}/>
+                 <NAV_Image src={MESSAGE}/>
+                 <NAV_Image src={SECRET}/>
+                </Black_Circle>
+            </Pink_Circle>
+        </White_Circle>
     );
 };
 
